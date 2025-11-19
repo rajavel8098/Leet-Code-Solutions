05db90d531ec5a1e651bc20196bfb133f9cea545
rajavel8098 <rajavels2005@gmail.com>
2025-11-19 19:20:09 +0530

    Time: 0 ms (100%), Space: 12.4 MB (85.79%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -4,39 +4,33 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-
-        INT_MIN = -2**31        
-        INT_MAX = 2**31 - 1     
-
+        
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+        
         i = 0
         n = len(s)
-
-       
+        
+      
         while i < n and s[i] == ' ':
             i += 1
-
         
-        if i == n:
-            return 0
-
-       
+        
         sign = 1
-        if s[i] == '-':
-            sign = -1
+        if i < n and (s[i] == '+' or s[i] == '-'):
+            sign = -1 if s[i] == '-' else 1
             i += 1
-        elif s[i] == '+':
-            i += 1
-
-       
+        
+        
         num = 0
         while i < n and s[i].isdigit():
             digit = ord(s[i]) - ord('0')
-
             
-            if num > (INT_MAX // 10) or (num == INT_MAX // 10 and digit > INT_MAX % 10):
+           
+            if num > (INT_MAX - digit) // 10:
                 return INT_MAX if sign == 1 else INT_MIN
-
+            
             num = num * 10 + digit
             i += 1
-
+        
         return num * sign
